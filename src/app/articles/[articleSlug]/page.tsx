bab3865500ec5e2cb13aa10f36366d115587a267
Raj Chavda <raj.theappideas@gmail.com>
2024-03-11 12:38:49 +0530

    error solved

---
+++ src/app/articles/[articleSlug]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticleSlug = (params:any) => {
+        console.log(params);
+        
+  return (
+    <div>article slug</div>
+  )
+}
+
+export default ArticleSlug
