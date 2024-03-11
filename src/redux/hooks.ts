7143b4429fc20fb7c6360b58812aa81037063928
Raj Chavda <raj.theappideas@gmail.com>
2024-03-11 11:17:42 +0530

    solved error

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,8 +1,18 @@
-import { AppDispatch, AppStore } from "@/redux/store";
+import { AppDispatch, AppStore, store } from "@/redux/store";
 import { RootState } from "./store";
 import { TypedUseSelectorHook, useDispatch } from "react-redux";
-import { useStore, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppStore: () => AppStore = useStore;
+export const useAppStore = () => {
+  const { reduxStore, persistor } = store(); // Assuming useStore is a hook returning the Redux store
+
+  // Explicitly define the return type to match the expected structure
+  const result: AppStore = {
+    reduxStore,
+    persistor,
+  };
+
+  return result;
+};
